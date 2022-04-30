3bfdc7d55ba2055928bbcc06be5e82173c713b25
Bank Surachet <Surachet.4129@gmail.com>
2022-04-30 10:36:10 +0700

    Created: New Project base on MVVM architectural pattern.

---
+++ CodingTest/NewsApp/NewsApp/NewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppApp.swift
+//  NewsApp
+//
+//  Created by Surachet Raksee on 30/4/2565 BE.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CodingTest/NewsApp/NewsApp/View/Home.swift
@@ -0,0 +1,20 @@
+//
+//  Home.swift
+//  NewsApp
+//
+//  Created by Surachet Raksee on 30/4/2565 BE.
+//
+
+import SwiftUI
+
+struct Home: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct Home_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
